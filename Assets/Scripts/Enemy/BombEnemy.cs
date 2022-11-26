18e55fa602b0a2fbe685182b1a56fecf7f8c30be
Ignolek <ignole@icloud.com>
2022-11-26 10:51:40 +0100

    Added BombEnemy

---
+++ Assets/Scripts/Enemy/BombEnemy.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BombEnemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform Player;
+    public float ExplosionDelay;
+
+    // Params
+    public float Health;
+    public float CurrentHealth;
+    public float attackRange;
+
+    public bool alreadyAttacked;
+
+    private void Awake()
+    {
+        CurrentHealth = Health;
+        Player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alreadyAttacked)
+        {
+            agent.destination = Player.transform.position;
+            ExplosionDelay -= Time.deltaTime;
+            Debug.Log(ExplosionDelay);
+
+            //agent.transform.localScale *= 0.1f * Time.deltaTime;
+            if (ExplosionDelay <= 0 || CurrentHealth <= 0)
+                DestroyEnemy();
+        }
+
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        if (distance <= attackRange)
+        {
+            alreadyAttacked = true;
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        CurrentHealth -= damage;
+        if (CurrentHealth <= 0)
+            Invoke(nameof(DestroyEnemy), 0.5f);
+    }
+
+    private void DestroyEnemy()
+    {
+        // Here BUUUUM code
+        Debug.Log("BUUUUUUUUM");
+        //
+        Destroy(gameObject);
+    }
+}
