81512052fd45c80c42454366cc89fb6883c9e677
Ignolek <ignole@icloud.com>
2022-11-27 12:22:35 +0100

    Added animations to Boss script

--- Assets/Scripts/Enemy/Boss.cs
+++ Assets/Scripts/Enemy/Boss.cs
@@ -7,7 +7,9 @@ public class Boss : MonoBehaviour
 {
     private NavMeshAgent agent;
     public Transform Player;
+    public Animator animator;
     public Transform ShootPoint;
+    public GameObject waveSpawner;
 
     //Params
     public float Health;
@@ -32,6 +34,9 @@ public class Boss : MonoBehaviour
         CurrentHealth = Health;
         Player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        if (animator == null)
+            Debug.LogError("NO ANIMATOR ON BOSS!");
     }
 
     // Start is called before the first frame update
@@ -44,47 +49,73 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.destination = Player.transform.position;
-        float distance = Vector3.Distance(agent.transform.position, Player.transform.position);
-
-        if (distance <= distanceAttackRange && distance > meleeAttackRange)
+        if (!waveSpawner.GetComponent<WaveSpawner>().inCombat)
         {
-            rangeAttack();
+            // Idle animation
+            animator.SetBool("Idle", true);
         }
-        else if (distance <= meleeAttackRange)
+        else
         {
-            meleeAttack();
+            animator.SetBool("Movement", true);
+            animator.SetBool("Idle", false);
+            agent.destination = Player.transform.position;
+            float distance = Vector3.Distance(agent.transform.position, Player.transform.position);
+
+            if (distance <= distanceAttackRange && distance > meleeAttackRange)
+            {
+                rangeAttack();
+            }
+            else if (distance <= meleeAttackRange)
+            {
+                meleeAttack();
+            }
         }
     }
 
     private void rangeAttack()
     {
-        rangeAttackCurrentTime -= Time.deltaTime;
         agent.SetDestination(transform.position);
         if (!alreadyAttacked)
         {
             // Play animation here (probably)
-
+            animator.SetBool("Movement", false);
+            animator.SetTrigger("Shoot");
             //
-            if (rangeAttackCurrentTime <= rangeAttackDelay)
-            {
-                Rigidbody rb = Instantiate(projectile, ShootPoint.transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-                rb.AddForce(ShootPoint.transform.forward * projectileSpeed, ForceMode.Impulse);
-                rangeAttackCurrentTime = rangeAttackDelay;
-                alreadyAttacked = true;
-            }
+
+            Rigidbody rb = Instantiate(projectile, ShootPoint.transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(ShootPoint.transform.forward * projectileSpeed, ForceMode.Impulse);
+            rangeAttackCurrentTime = rangeAttackDelay;
+            alreadyAttacked = true;
         }
     }
     private void meleeAttack()
     {
-        meleeAttackCurrentTime -= Time.deltaTime;
         agent.SetDestination(transform.position);
 
         if (!alreadyAttacked)
         {
             // Play animation here (probably)
-
+            animator.SetBool("Movement", false);
+            animator.SetTrigger("Damage");
             //
+            alreadyAttacked = true;
         }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        CurrentHealth -= damage;
+        if (animator != null)
+            animator?.SetTrigger("Damage");
+
+        if (CurrentHealth <= 0)
+        {
+            Invoke(nameof(DestroyEnemy), 0.5f);
+        }
+    }
+
+    private void DestroyEnemy()
+    {
+        Destroy(gameObject);
     }
 }
